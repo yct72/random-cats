e8e12b1bec13f148377a2c51554f805aebcfad6e
vickie <is.vickie18@gmail.com>
2021-08-12 19:14:32 +0800

    firebase finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import GeneratorBtn from "./components/GeneratorBtn";
+import LikeBtn from "./components/LikeBtn";
 import Title from "./components/Title";
 
-const App = () => {
+const App = ({ db, initCount }) => {
     return(
         <div>
             <Title/>
+            <LikeBtn db={ db } initCount={ initCount }/>
             <GeneratorBtn/>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,12 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/app';
+import 'firebase/database';
+const firebaseConfig = {
+  apiKey: "AIzaSyBmh9RGibDB6MujyzXoS2t0gUk4hZWsDmE",
+  authDomain: "random-cats-fc969.firebaseapp.com",
+  databaseURL: "https://random-cats-fc969-default-rtdb.firebaseio.com",
+  projectId: "random-cats-fc969",
+  storageBucket: "random-cats-fc969.appspot.com",
+  messagingSenderId: "427970997751",
+  appId: "1:427970997751:web:a168e000ddca6bdcfd52f2"
+};
+firebase.initializeApp(firebaseConfig);
+var db = firebase.database();
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+var initCount;
+const init = () => {
+  db.ref("/like").once("value", e => {
+    initCount = e.val();
+  }).then(() => {
+    laterLoad()
+  })
+}
+const laterLoad = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App db={ db } initCount={ initCount }/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+window.onload = init;
 
 
